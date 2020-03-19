5d381f3788d734e426e7afa1021b538fe7f78318
Joshua Fung <js404839@dal.ca>
2020-03-19 18:58:37 -0300

    Created a service to get JSON data from github API call. Displayed the information on the projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GithubService} from "../github.service";
 
 @Component({
   selector: 'app-projects',
@@ -7,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  //Will store the JSON data from github
+  json: any = [];
+
+  //Need to pass in the service so that we can use it
+  constructor(private service: GithubService) {
+    this.getData();
+  }
+
+  //Gets the data from github and puts it in the JSON variable
+  getData(){
+    this.service.getGithubInfo().subscribe(data => {
+      //Loops through the data and gets each item as needed
+      for(const d of (data as any)){
+        this.json.push({
+          name: d.name,
+          description: d.description,
+          language: d.language
+        });
+      }
+      //Just outputs to the console to make sure we got the right data
+      console.log(this.json);
+    });
+  }
 
   ngOnInit(): void {
   }
